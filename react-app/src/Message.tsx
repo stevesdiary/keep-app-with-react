a7a2d6706d5b1e930f04d779c1899e6f94d82aa4
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-04-29 08:28:27 +0100

    Initiate react lesson

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,9 @@
+//Follow PascalCase convention for component names
+
+function Message() {
+  //Jsx javascript xml
+  return <h1>Hello React!</h1>;
+}
+
+export default Message;
+// This is a functional component in React that returns a simple JSX element.
