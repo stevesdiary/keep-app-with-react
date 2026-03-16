9c27258fc421aa0ee66fc0d43004e82b7bafe881
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2026-03-16 17:10:26 +0100

    Remove react-app scaffold
    Delete the react-app scaffold including source files, public assets, Vite/TypeScript configs, package.json and package-lock.json. Also update .DS_Store. This removes the example Vite + React + TypeScript application from the repository.

--- react-app/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import ListGroup from "./components/ListGroup";
-
-function App() {
-  return <div><ListGroup /></div>;
-}
-
-export default App;
-// This is the main application component that imports and uses the Message component.
